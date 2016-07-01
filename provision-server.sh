b5ebe4993544cc7c1b2687a4625ebc74cf13365e
Will Stern <willrstern@gmail.com>
2016-07-01 12:26:53 -0500

    updated provisioning

--- provision-server.sh
+++ provision-server.sh
@@ -8,18 +8,19 @@
 # - ssh in again, this time with -t -t to allow nvm commands
 # - istall a node.js version
 # - install pm2
-ssh root@104.236.127.216 <<'EOF'
+ssh root@104.236.192.176 <<'EOF'
   mkdir app && mkdir app/current
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh | bash
   apt-get update && apt-get install -y git
+  ssh-keyscan github.com >> ~/.ssh/known_hosts
 EOF
 
-ssh -t -t root@104.236.127.216 <<'EOF'
+ssh -t -t root@104.236.192.176 <<'EOF'
   nvm install 6
   npm i -g pm2
   exit
 EOF
 
 ## add your private ssh key to semaphore and run these commands for deployment
-##    ssh-keyscan -H -p 22 104.236.127.216 >> ~/.ssh/known_hosts
-##    ssh -A -t -t root@104.236.127.216 'bash -s' < deploy.sh
+##    ssh-keyscan -H -p 22 104.236.192.176 >> ~/.ssh/known_hosts
+##    ssh -A -t -t root@104.236.192.176 'bash -s' < deploy.sh
