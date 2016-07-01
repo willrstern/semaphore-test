bef8b9dbf5bff208dadef88a2e3b6ee89404b020
Will Stern <willrstern@gmail.com>
2016-07-01 11:30:30 -0500

    update provision instructions

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-# chmod +x deploy.sh
-
-##provision server
-## install nvm then log out and back in
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh | bash
-
-
-
-## install git
-# apt-get install git
-
-## add a git ssh key
-# echo "<key content>" > ~/.ssh/id_rsa
-
-## clone inital repo
-# git clone git@github.com:willrstern/semaphore-test.git current
-# cd current && nvm install
-# npm i -g pm2
-# ssh-keyscan -H -p 22 159.203.81.98 >> ~/.ssh/known_hosts
-# ssh -A -t -t root@159.203.81.98 'bash -s' < deploy.sh
-# ssh -t -t root@159.203.81.98 << 'EOF'
 cd app
 mv current backup
 git clone git@github.com:willrstern/semaphore-test.git current
@@ -27,4 +6,3 @@ cd current && nvm install && nvm use && npm install
 pm2 stop all && pm2 start app.js -i 0
 rm -rf ../backup
 exit
-# EOF
---
+++ provision-server.sh
@@ -0,0 +1,20 @@
+#provision server
+# ssh into server
+# install nvm then log out
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh | bash
+
+# ssh with -A flag to forward our ssh keys
+# install git
+apt-get install -y git
+
+# clone inital repo
+git clone git@github.com:willrstern/semaphore-test.git current
+
+# install current .nvmrc version of node
+cd current && nvm install
+# install pm2
+npm i -g pm2
+
+# add your private ssh key to semaphore and run these commands for deployment
+ssh-keyscan -H -p 22 <ip address> >> ~/.ssh/known_hosts
+ssh -A -t -t root@<ip address> 'bash -s' < deploy.sh
